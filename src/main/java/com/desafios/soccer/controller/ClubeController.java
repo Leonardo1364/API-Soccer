9d1c257bf50b2c788d4b9874d3f76ae297a8b621
Leonardo1364 <leo_lmota@hotmail.com>
2021-10-14 23:41:46 -0300

    implementando clube service, repository, controller

---
+++ src/main/java/com/desafios/soccer/controller/ClubeController.java
@@ -0,0 +1,11 @@
+package com.desafios.soccer.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/v1/clube")
+public class ClubeController {
+}
