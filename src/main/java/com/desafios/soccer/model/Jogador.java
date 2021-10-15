4ab967fca837d8a8cb62e3c26970e7b1939ec2f1
Giovane <giovanefernandes35@gmail.com>
2021-10-14 22:09:47 -0300

    Add dependencies

--- src/main/java/com/desafios/soccer/model/Jogador.java
+++ src/main/java/com/desafios/soccer/model/Jogador.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class Jogador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private Integer idade;
--- src/main/java/com/desafios/soccer/service/JogadorService.java
+++ src/main/java/com/desafios/soccer/service/JogadorService.java
@@ -1,8 +1,21 @@
 package com.desafios.soccer.service;
 
+import com.desafios.soccer.model.Clube;
+import com.desafios.soccer.model.Jogador;
+import com.desafios.soccer.repository.JogadorRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class JogadorService {
+
+    private final JogadorRepository jogadorRepository;
+
+
+
+
+
+
 
 }
