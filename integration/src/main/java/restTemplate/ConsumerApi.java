529192d79221304c5663e302ce54d404c3a4f2ab
Leonardo1364 <leo_lmota@hotmail.com>
2022-01-19 14:10:12 -0300

    implementation ResTemplate class

--- integration/src/main/java/restTemplate/ConsumerApi.java
+++ integration/src/main/java/restTemplate/ConsumerApi.java
@@ -3,25 +3,30 @@ package restTemplate;
 import com.desafios.soccer.model.entity.League;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @AllArgsConstructor
-@Service
+@Component
 public class ConsumerApi {
+
+    private RestTemplate restTemplate;
 
     @Bean
     public League findLeague(League league) {
-        String uri = "http://localhost:27018/v1/league" + league.getName() + "/json/";
-
-        /*UriComponents uri = UriComponentsBuilder.newInstance()
-                .scheme("http")
-                .host("localhost:27018")
-                .path("v1/league")
-                .build();*/
-        RestTemplate rs = new RestTemplate();
-        return rs.getForObject(uri, League.class);
+        String uri = "http://localhost:8081/v1/league" + league.getName() + "/json/";
+        return restTemplate.getForObject(uri, League.class);
     }
 
+    @Bean
+    public League save(League league) {
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("localhost:8081")
+                .path("v1/league")
+                .build();
+        return restTemplate.postForObject(uri.toUri(), league.getName(), League.class);
+    }
 }
